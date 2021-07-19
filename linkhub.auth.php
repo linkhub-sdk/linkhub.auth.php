732e17ed045d4c76d757d883315f32e915c630b8
1Jae <wjkim@linkhub.co.kr>
2021-07-19 14:45:59 +0900

    fixed updated date

--- linkhub.auth.php
+++ linkhub.auth.php
@@ -12,7 +12,7 @@
 * Author : Kim Seongjun (pallet027@gmail.com)
 * Contributor : Jeong Yohan (code@linkhub.co.kr)
 * Written : 2017-08-29
-* Updated : 2020-10-07
+* Updated : 2021-06-28
 *
 * Thanks for your interest.
 * We welcome any suggestions, feedbacks, blames or anythings.
